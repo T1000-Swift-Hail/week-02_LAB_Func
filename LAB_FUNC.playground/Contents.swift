bb4a3c78b3fa1abe8a674da870319c27e215dac5
Seham الشطنان <sehamalshtnan@Sehams-MacBook-Air.local>
2021-10-10 11:15:06 +0300

    lap

---
+++ LAB_FUNC.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+func printMyName(name:String,age:Int)->String{
+ return"this is my name :\(name)this is my age:\(age)"
+    
+}
+
+var phrase = printMyName(name: "Seham",age:29)
+print(phrase)
+
+
+
+
+
+
