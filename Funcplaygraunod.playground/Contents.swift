c5f59af3bcbe071ce5198ec566c4fc1811af8994
MACBOOK <macbook@Az-MacBook-Pro.local>
2021-10-10 10:20:23 +0300

    the Lab Func done

---
+++ Funcplaygraunod.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Foundation
+
+func printMyName(MyName Name:String,Age:Int) -> String {
+    
+  return "This is my Name: \(Name) and This is My Age: \(Age)"
+    
+    
+}
+
+let NameAndAge = printMyName(MyName: "Abdulaziz", Age: 26)
+
+print(NameAndAge)
+
+
+
+
+
+            
+            
