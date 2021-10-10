6b2f263d381ead0714b8de532c572ed68213c42b
Mastorah <mas@Mastorahs-MacBook-Air.local>
2021-10-10 10:43:20 +0300

    Finish

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+func getMyName (forName name: String ,myAge age :Int )-> String {
+
+return "My name is: \(name) and myage : \(age) years."}
+  
+ let name1 = getMyName (forName: "Mastorh", myAge: 28)
+print(name1)
