eaa1bceccc0f59310af18a95a8f85a23d57d99f1
Majed Alshammari <admin@Majeds-MacBook-Air.local>
2021-10-10 10:58:46 +0300

    Lab func Edited

--- LAB_FUNC.playground/Contents.swift
+++ LAB_FUNC.playground/Contents.swift
@@ -1,9 +1,9 @@
 import UIKit
 
-func printMyName (forName name: String, Age:Int)  {
-    
-    print("My name is \(name) and I'm \(Age) years old")
-    return
+func printMyName (forName name: String, Age: Int) -> String  {
+    guard Age == 24 else {return "wrong age"}
+     return "My name is \(name) and I'm \(Age) years old"
 }
 
-printMyName(forName: "Majed", Age: 24)
+let MyName = printMyName(forName: "Majed", Age: 24)
+print(MyName)
