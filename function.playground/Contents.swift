6d4edcb4e43c4e02692992bcc3928c2cbd45e952
azoz <aabdoalaziz1@gmail.com>
2021-10-10 10:43:46 +0300

    Lab Function is Done

---
+++ function.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+func getMyName(forMyGetName name: String, Age:Int) -> String {
+return "my name is \(name) and my age is \(Age)"
+}
+
+let theGetMyName = getMyName(forMyGetName:"Abdoalaziz",Age: 28 )
+print(theGetMyName)
+
+    
+
+
