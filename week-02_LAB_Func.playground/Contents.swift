b5c5735745a7212b0a7c1a7ee522e06b6e674207
hanaalmutlak <Hana.Khamis.has.s@tuwaiq.edu.sa>
2021-10-10 11:01:01 +0300

    solve

---
+++ week-02_LAB_Func.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+func printMyName (name: String, age: Int) -> String {
+    return "my name is \(name) and I am  \(age) old"
+}
+
+let Myname = printMyName(name: "Hana" , age : 26)
+print(Myname)
+
+
