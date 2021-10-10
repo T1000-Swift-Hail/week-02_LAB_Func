b11e5a45db07bfa0ca12c950768242898c68422e
hesah <hesah.raja.has.s@tuwaiq.edu.sa>
2021-10-10 10:25:44 +0300

    done func

---
+++ LAB_Func.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+func printMyName(forName name:String , MyAge:Int )  {
+    
+    print("I am \(name) and I am \(MyAge) years old")
+}
+
+
+
+printMyName(forName: "Hesah", MyAge: 28)
