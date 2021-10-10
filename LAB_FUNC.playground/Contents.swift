6eab37e1d1bf50bdb4b7d079d77503881b0744c7
Majed Alshammari <admin@Majeds-MacBook-Air.local>
2021-10-10 10:10:42 +0300

    Lab func done

---
+++ LAB_FUNC.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+func printMyName (forName name: String, Age:Int)  {
+    
+    print("My name is \(name) and I'm \(Age) years old")
+    return
+}
+
+printMyName(forName: "Majed", Age: 24)
