5ed5a46042f607822180d37f84b3ccf188c07ec5
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-10 10:07:07 +0300

    Soling👍🏻

---
+++ LAB_FUNC.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+
+func printMyName (YourName name: String , YourAge : Int) -> String{
+//return "My name is \(name )"
+    return "My name is \(name) and I am \(YourAge) years old."
+}
+//let MyName = printMyName(name:" Monafh")
+let MyName = printMyName(YourName: "Monafh", YourAge: 2021 )
+
+print(MyName)
