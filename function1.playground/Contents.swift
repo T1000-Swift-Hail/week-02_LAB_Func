2a804dad32f4b5bb0c282251e330b5810b617cdb
AsmaRashed1 <Asma.Rashed.has.s@tuwaiq.edu.sa>
2021-10-10 10:29:17 +0300

    Done

---
+++ function1.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+func printMyName (forName name :String  ,  myAge age  :Int) -> (String) {
+    
+    return "My name is : \(name)  and I am : \(age)years old."}
+
+
+let myName1 = printMyName(forName: "Asma", myAge:28)
+
+
+print(myName1)
+    
