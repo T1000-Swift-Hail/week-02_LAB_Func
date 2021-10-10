49d406766f757ce9df5b788be8eda4d6eac50020
Anas Hamad <anashamad@MacBook-Air-Anas.local>
2021-10-10 10:29:37 +0300

    done

--- Func LaB.playground/Contents.swift
+++ Func LaB.playground/Contents.swift
@@ -1,3 +1,8 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func printMyName (Name forName: String, Age myAge:Int ) -> String {
+     
+     return( " My Name is: \(forName)... and my Age is :\(myAge)")
+    
+             }
+             print(printMyName(Name: "Anas", Age: 29))
