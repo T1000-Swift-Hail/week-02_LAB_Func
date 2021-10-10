e9a829aabe12338c49129b7c3c9f9cfa5dccdc7a
mac <Asma.Abdullah.has.s@tuwaiq.edu.sa>
2021-10-10 10:23:24 +0300

    LAB_Func

---
+++ LAB_FUNC.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+func printMyName (forName name: String, age: Int) -> String {
+    
+    
+    return "My name is \(name) and I am \(age) years old."
+}
+
+
+let MyName = printMyName(forName: "Asma", age: 24)
+print(MyName)
