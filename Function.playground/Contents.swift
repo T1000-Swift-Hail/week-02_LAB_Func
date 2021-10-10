37b3713cb4efe5bd3b042bb47360dd14370b8cc3
Hind Alharbi <Hind.Alharbi.has.s@tuwaiq.edu.sa>
2021-10-10 10:05:46 +0300

    week-02_LAB_Func

---
+++ Function.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+// Hind Alharbi
+
+func printMyName(forName name: String, age: Int){
+    
+    print("My Name is \(name) and I am \(age) years old")
+    
+}
+printMyName(forName: "Hind", age: 28)
