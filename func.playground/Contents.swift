e06650bf797faa044c56d60eefb5def77bf999d6
user <user@MacBook-Air-.local>
2021-10-10 10:34:51 +0300

    Lab Func is Done

---
+++ func.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+func printMyName(forName name: String,MyAge:Int) {
+    print("My name is: \(name) and i am \(MyAge) years:")
+return
+}
+
+
+
+ printMyName(forName: "Abdullah", MyAge: 24)
+
+                          
