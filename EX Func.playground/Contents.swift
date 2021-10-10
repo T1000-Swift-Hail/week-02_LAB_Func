b506e57f9f2a064010605e085691fa7fe8c6bc7a
طلال عبيدالله دعيع القلادي <talalalglaadi@MacBook-Air-tlal.local>
2021-10-10 11:28:56 +0300

    EX Func Done

---
+++ EX Func.playground/Contents.swift
@@ -0,0 +1,17 @@
+func printMyName(ForName: String){
+    print("My Name Is MR.\(ForName)")
+}
+printMyName(ForName: "Talal")
+
+
+
+func printMyName(ForName name: String? = "Unknown",MidlleName:String,age:Int)-> String {
+    
+    guard age == 29 else {return "Erorr Age !"}
+    
+    return " My Name Is :\(name) and My age is :\(age)"
+}
+
+let MyName = printMyName(ForName: "Talal", MidlleName:"Obaidallah" , age: 29)
+print(MyName)
+
