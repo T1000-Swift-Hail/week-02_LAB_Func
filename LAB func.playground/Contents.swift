76d1a3034551756d963b05fc6a0eecebca09a177
موضي الحربي <modhialhrbi@MacBook-Air-mwdy.local>
2021-10-10 10:41:28 +0300

    DN LAB FUNC

---
+++ LAB func.playground/Contents.swift
@@ -0,0 +1,10 @@
+
+func printMyName (forname name:String , age :Int) -> String {
+    
+    return "my name is: \(name) and my age is: \(age)"
+}
+
+let my = printMyName(forname: "Modhi", age : 35)
+print(my)
+
+
