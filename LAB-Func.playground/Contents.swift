958244e05f55d3b20de4c5c414968e3c12176a6d
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-10-10 11:29:46 +0300

    Done Lab

---
+++ LAB-Func.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+import Foundation
+
+func printMyName(name:String, age:Int) -> String{
+    return "This is my name :\(name)  this is my age : \(age) "
+
+}
+
+
+var phrase = printMyName(name: "Hind", age: 28)
+
+
+print(phrase)
+
+
+
