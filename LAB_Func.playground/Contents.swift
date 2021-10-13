30b8ce7c575e1d7e2c0aa2a54953bdeca2054058
Aryaf AlQabali <Aryaf.Aqeel.has.s@tuwaiq.edu.sa>
2021-10-13 16:22:59 +0300

    after editing

--- LAB_Func.playground/Contents.swift
+++ LAB_Func.playground/Contents.swift
@@ -1,8 +1,16 @@
 import UIKit
 
-func printMyName(name: String , age: Int){
-   print ("This is my name:\(name) and the age is:\(age)")
+func printMyName(name: String , age: Int)  -> String{
+   return "This is my name:\(name) and the age is:\(age)"
+  
+    
+        
 }
 
+var MyName = printMyName(name: "Aryaf", age: 23)
 
-printMyName(name: "Aryaf", age: 23)
+print(MyName)
+
+//print("This is my name:\(name) and the age is:\(age)")
+
+//printMyName(name: "Aryaf", age: "23")
