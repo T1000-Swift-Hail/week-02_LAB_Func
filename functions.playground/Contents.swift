68e8107dfcaf8f8e3da571c9026b8ea8f1f47592
Munira abdullah <muniraabdullah@MacBook-alkhas-b-Munira.local>
2021-10-13 16:13:54 +0300

    D

---
+++ functions.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+func printMyName (forName name : String , age: Int) -> String {
+    return "My name is :\(name) and I am \(age) years old "
+}
+let myName = printMyName(forName: "Munira", age : 24 )
+print(myName)
